6b6f730496ddb6eb1a7ca83589d725136fe56efc
Sulton <muhamadsulton217@gmail.com>
2022-11-10 06:50:30 +0700

    Add Unit Test

---
+++ tests/Feature/ProductUnitTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Product;
+
+
+class ProductUnitTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+    public function testInsert()
+    {
+        $dummydata = ["name" => "UnitTestSampleName1","detail" => "UnitTestSampleDetail1"];
+        $response = $this->withHeaders($this->login())->json('POST', '/api/products/store',$dummydata);
+        $response->assertJsonFragment(["success" => true]);
+    }
+
+    public function testList()
+    {
+        $response = $this->withHeaders($this->login())->json('GET', '/api/products/');
+        $response->assertJsonFragment(["name" => "UnitTestSampleName1"]);
+    }
+
+    public function testShowByID()
+    {
+        $lastid=Product::latest('id')->pluck('id')->first();
+        $response = $this->withHeaders($this->login())->json('GET', '/api/products/show/'.$lastid);
+        $response->assertJsonFragment(["name" => "UnitTestSampleName1"]);
+    }
+
+    public function testUpdate()
+    {
+        $lastid=Product::latest('id')->pluck('id')->first();
+        $dummydata = ["name" => "UnitTestSampleName2","detail" => "UnitTestSampleDetail2"];
+        $response = $this->withHeaders($this->login())->json('PUT', '/api/products/update/'.$lastid,$dummydata);
+        $response->assertJsonFragment(["success" => true]);
+    }
+
+    public function testDeleteByID()
+    {
+        $getdummy=Product::where('name','UnitTestSampleName1')->latest('id')->pluck('id')->first();
+        $response = $this->withHeaders($this->login())->json('delete', '/api/products/delete/'.$getdummy);
+        $response->assertJsonFragment(["message" => "Product deleted successfully."]);
+        Product::where('name','UnitTestSampleName1')->delete();
+        Product::where('name','UnitTestSampleName2')->delete();
+    }
+
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace Tests;
+use App\Models\User;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    public $jwtToken = [];
+    public function login($user = "admin@gmail.com")
+    {
+        if (empty($this->jwtToken[$user])) {
+            $this->jwtToken[$user] = auth()->login(User::where("email", $user)->first());
+        }
+        return  ['Authorization' => 'Bearer ' . $this->jwtToken[$user]];
+    }
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutExceptionHandling();
+    }
 }
