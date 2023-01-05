c6770967dc47423dc3ea73cadddd6934c5868c7c
Sulton <agus.puryanto@kpvendor.id>
2023-01-05 13:40:28 +0700

    Add Factory / faker unit test

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Product::class;
+
+    
+    public function definition()
+    {
+        return [
+            //
+            'name' => $this->faker->sentence(2),
+            'detail' => $this->faker->paragraph(10),
+
+        ];
+    }
+}
